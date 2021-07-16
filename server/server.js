aa78764b3f097b8deddb2e63f2b5b48a237824e0
jojo <jodiladouceur.design@gmail.com>
2021-07-16 09:04:43 -0400

    edits to files

--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
+const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
 });
 
 server.applyMiddleware({ app });
@@ -28,10 +25,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
